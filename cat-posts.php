76484d22b3e0610d31f2ebfd0757db1afe203de5
Hugues Deraps <hderaps@yahoo.com>
2015-07-10 16:17:07 +1000

    A little Light version

--- cat-posts.php
+++ cat-posts.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: Category Posts Widget
-Plugin URI: http://mkrdip.me/category-posts-widget
+Plugin Name: Latest Posts Widget - A Little Light version
+Plugin URI: https://wordpress.org/plugins/category-posts/
 Description: Adds a widget that shows the most recent posts from a single category.
-Author: Mrinal Kanti Roy
-Version: 4.0
-Author URI: http://mkrdip.me
+Author: Mrinal Kanti Roy and Adaptation for the A Little Light Theme by hderaps.
+Version: 4.1
+Author URI: http://profiles.wordpress.org/mkrdip/
 */
 
 // Don't call the file directly
@@ -57,102 +57,99 @@ class CategoryPosts extends WP_Widget {
 	function widget($args, $instance) {
 		global $post;
 		$post_old = $post; // Save the post object.
-
+		
 		extract( $args );
-
+		
 		$sizes = get_option('mkrdip_cat_post_thumb_sizes');
 		
 		// If not title, use the name of the category.
 		if( !$instance["title"] ) {
 			$category_info = get_category($instance["cat"]);
-			$instance["title"] = $category_info->name;		
+			$instance["title"] = $category_info->name;
+	  }
 
-		$valid_sort_orders = array('date', 'title', 'comment_count', 'rand');
-		if ( in_array($instance['sort_by'], $valid_sort_orders) ) {
-			$sort_by = $instance['sort_by'];
-			$sort_order = (bool) isset( $instance['asc_sort_order'] ) ? 'ASC' : 'DESC';
-		} else {
-			// by default, display latest first
-			$sort_by = 'date';
-			$sort_order = 'DESC';
-		}
-
+	  $valid_sort_orders = array('date', 'title', 'comment_count', 'rand');
+	  if ( in_array($instance['sort_by'], $valid_sort_orders) ) {
+		$sort_by = $instance['sort_by'];
+		$sort_order = (bool) isset( $instance['asc_sort_order'] ) ? 'ASC' : 'DESC';
+	  } else {
+		// by default, display latest first
+		$sort_by = 'date';
+		$sort_order = 'DESC';
+	  }
+		
 		// Get array of post info.
-		$cat_posts = new WP_Query(
-			"showposts=" . $instance["num"] . 
-			"&cat=" . $instance["cat"] .
-			"&orderby=" . $sort_by .
-			"&order=" . $sort_order
-		);
+	  $cat_posts = new WP_Query(
+		"showposts=" . $instance["num"] . 
+		"&cat=" . $instance["cat"] .
+		"&orderby=" . $sort_by .
+		"&order=" . $sort_order
+	  );
 
 		// Excerpt length filter
 		$new_excerpt_length = create_function('$length', "return " . $instance["excerpt_length"] . ";");
 		if ( $instance["excerpt_length"] > 0 )
 			add_filter('excerpt_length', $new_excerpt_length);
-
+		
 		echo $before_widget;
-
+		
 		// Widget title
 		echo $before_title;
-		if( $instance["title_link"] ) {
+		if( isset( $instance["title_link"] ) )
 			echo '<a href="' . get_category_link($instance["cat"]) . '">' . $instance["title"] . '</a>';
-		} else {
+		else
 			echo $instance["title"];
-		}
 		echo $after_title;
 
 		// Post list
 		echo "<ul>\n";
-
+		
 		while ( $cat_posts->have_posts() )
 		{
 			$cat_posts->the_post();
 		?>
-			<li class="cat-post-item">
-				<?php if ( !is_single( get_the_title() ) ) : ?>
-					<a class="post-title" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-				<?php else : ?>
-					<a class="post-title current-post" href="<?php the_permalink(); ?>" rel="bookmark"><?php the_title(); ?></a>
-				<?php endif; ?>
-
+			<li class="cat-post-item col col-12 no-gutter">
+           
+				<?php if ( in_category ('work-life-balance')) :?><i class="icon-scales"></i><?php elseif ( in_category ('behind-the-scenes')) :?><i class="icon-dna"></i><?php elseif ( in_category ('inspired-by')) :?><i class="icon-head-idea-lightbulb"></i><?php elseif ( in_category ('on-small-business')) :?><i class="icon-feather"></i><?php elseif ( in_category ('making-a-home')) :?><i class="icon-tools"></i><?php else:?><i class="icon-pin"></i><?php endif ?> <a class="post-title" href="<?php the_permalink(); ?>" rel="bookmark" title="Permanent link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+				
 
 				<?php if ( isset( $instance['date'] ) ) : ?>
-				<p class="post-date"><?php the_time("j M Y"); ?></p>
+				&nbsp;&nbsp;<span class="post-date"><?php the_time("j M Y"); ?></span>
 				<?php endif; ?>
-
+				
 				<?php
 					if (
 						function_exists('the_post_thumbnail') &&
 						current_theme_supports("post-thumbnails") &&
 						isset( $instance["thumb"] ) &&
 						has_post_thumbnail()
+
 					) :
 				?>
 					<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
 					<?php the_post_thumbnail( 'cat_post_thumb_size'.$this->id ); ?>
 					</a>
 				<?php endif; ?>
-
+							
 				<?php if ( isset( $instance['excerpt'] ) ) : ?>
 				<?php the_excerpt(); ?> 
 				<?php endif; ?>
-
+				
 				<?php if ( isset( $instance['comment_num'] ) ) : ?>
 				<p class="comment-num">(<?php comments_number(); ?>)</p>
 				<?php endif; ?>
 			</li>
 			<?php
 		}
-
+		
 		echo "</ul>\n";
-
+		
 		echo $after_widget;
 
 		remove_filter('excerpt_length', $new_excerpt_length);
-
-		if (function_exists ('wp_reset_postdata')) //wp_reset_postdata only exists in WordPress >3.0.0
-			wp_reset_postdata();
-
+		
+		wp_reset_postdata();
+	
 	}
 
 	/**
@@ -164,14 +161,14 @@ class CategoryPosts extends WP_Widget {
 	 */
 	function update($new_instance, $old_instance) {
 		$sizes = get_option('mkrdip_cat_post_thumb_sizes');
-
+			
 		if ( !$sizes ) {
 			$sizes = array();
 		}
-
+		
 		$sizes[$this->id] = array($new_instance['thumb_w'], $new_instance['thumb_h']);
 		update_option('mkrdip_cat_post_thumb_sizes', $sizes);
-
+				
 		return $new_instance;
 	}
 
@@ -184,11 +181,11 @@ class CategoryPosts extends WP_Widget {
 	function form($instance) {
 		$instance = wp_parse_args( ( array ) $instance, array(
 			'title'          => __( '' ),
-			'cat'            => __( '' ),
+			'cat'			 => __( '' ),
 			'num'            => __( '' ),
 			'sort_by'        => __( '' ),
 			'asc_sort_order' => __( '' ),
-			'title_link'     => __( '' ),
+			'title_link'	 => __( '' ),
 			'excerpt'        => __( '' ),
 			'excerpt_length' => __( '' ),
 			'comment_num'    => __( '' ),
@@ -199,11 +196,11 @@ class CategoryPosts extends WP_Widget {
 		) );
 
 		$title          = $instance['title'];
-		$cat            = $instance['cat'];
+		$cat 			= $instance['cat'];
 		$num            = $instance['num'];
 		$sort_by        = $instance['sort_by'];
 		$asc_sort_order = $instance['asc_sort_order'];
-		$title_link     = $instance['title_link'];
+		$title_link		= $instance['title_link'];		
 		$excerpt        = $instance['excerpt'];
 		$excerpt_length = $instance['excerpt_length'];
 		$comment_num    = $instance['comment_num'];
@@ -211,7 +208,7 @@ class CategoryPosts extends WP_Widget {
 		$thumb          = $instance['thumb'];
 		$thumb_w        = $instance['thumb_w'];
 		$thumb_h        = $instance['thumb_h'];
-
+				
 			?>
 			<p>
 				<label for="<?php echo $this->get_field_id("title"); ?>">
@@ -219,68 +216,78 @@ class CategoryPosts extends WP_Widget {
 					<input class="widefat" id="<?php echo $this->get_field_id("title"); ?>" name="<?php echo $this->get_field_name("title"); ?>" type="text" value="<?php echo esc_attr($instance["title"]); ?>" />
 				</label>
 			</p>
+			
 			<p>
 				<label>
 					<?php _e( 'Category' ); ?>:
 					<?php wp_dropdown_categories( array( 'name' => $this->get_field_name("cat"), 'selected' => $instance["cat"] ) ); ?>
 				</label>
 			</p>
+			
 			<p>
 				<label for="<?php echo $this->get_field_id("num"); ?>">
 					<?php _e('Number of posts to show'); ?>:
 					<input style="text-align: center;" id="<?php echo $this->get_field_id("num"); ?>" name="<?php echo $this->get_field_name("num"); ?>" type="text" value="<?php echo absint($instance["num"]); ?>" size='3' />
 				</label>
 			</p>
+
 			<p>
 				<label for="<?php echo $this->get_field_id("sort_by"); ?>">
 					<?php _e('Sort by'); ?>:
 					<select id="<?php echo $this->get_field_id("sort_by"); ?>" name="<?php echo $this->get_field_name("sort_by"); ?>">
-						<option value="date"<?php selected( $instance["sort_by"], "date" ); ?>>Date</option>
-						<option value="title"<?php selected( $instance["sort_by"], "title" ); ?>>Title</option>
-						<option value="comment_count"<?php selected( $instance["sort_by"], "comment_count" ); ?>>Number of comments</option>
-						<option value="rand"<?php selected( $instance["sort_by"], "rand" ); ?>>Random</option>
+					  <option value="date"<?php selected( $instance["sort_by"], "date" ); ?>>Date</option>
+					  <option value="title"<?php selected( $instance["sort_by"], "title" ); ?>>Title</option>
+					  <option value="comment_count"<?php selected( $instance["sort_by"], "comment_count" ); ?>>Number of comments</option>
+					  <option value="rand"<?php selected( $instance["sort_by"], "rand" ); ?>>Random</option>
 					</select>
 				</label>
 			</p>
+			
 			<p>
 				<label for="<?php echo $this->get_field_id("asc_sort_order"); ?>">
 					<input type="checkbox" class="checkbox" 
-						id="<?php echo $this->get_field_id("asc_sort_order"); ?>" 
-						name="<?php echo $this->get_field_name("asc_sort_order"); ?>"
-						<?php checked( (bool) $instance["asc_sort_order"], true ); ?> />
+					  id="<?php echo $this->get_field_id("asc_sort_order"); ?>" 
+					  name="<?php echo $this->get_field_name("asc_sort_order"); ?>"
+					  <?php checked( (bool) $instance["asc_sort_order"], true ); ?> />
 							<?php _e( 'Reverse sort order (ascending)' ); ?>
 				</label>
 			</p>
+
 			<p>
 				<label for="<?php echo $this->get_field_id("title_link"); ?>">
 					<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id("title_link"); ?>" name="<?php echo $this->get_field_name("title_link"); ?>"<?php checked( (bool) $instance["title_link"], true ); ?> />
 					<?php _e( 'Make widget title link' ); ?>
 				</label>
 			</p>
+			
 			<p>
 				<label for="<?php echo $this->get_field_id("excerpt"); ?>">
 					<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id("excerpt"); ?>" name="<?php echo $this->get_field_name("excerpt"); ?>"<?php checked( (bool) $instance["excerpt"], true ); ?> />
 					<?php _e( 'Show post excerpt' ); ?>
 				</label>
 			</p>
+			
 			<p>
 				<label for="<?php echo $this->get_field_id("excerpt_length"); ?>">
 					<?php _e( 'Excerpt length (in words):' ); ?>
 				</label>
 				<input style="text-align: center;" type="text" id="<?php echo $this->get_field_id("excerpt_length"); ?>" name="<?php echo $this->get_field_name("excerpt_length"); ?>" value="<?php echo $instance["excerpt_length"]; ?>" size="3" />
 			</p>
+			
 			<p>
 				<label for="<?php echo $this->get_field_id("comment_num"); ?>">
 					<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id("comment_num"); ?>" name="<?php echo $this->get_field_name("comment_num"); ?>"<?php checked( (bool) $instance["comment_num"], true ); ?> />
 					<?php _e( 'Show number of comments' ); ?>
 				</label>
 			</p>
+			
 			<p>
 				<label for="<?php echo $this->get_field_id("date"); ?>">
 					<input type="checkbox" class="checkbox" id="<?php echo $this->get_field_id("date"); ?>" name="<?php echo $this->get_field_name("date"); ?>"<?php checked( (bool) $instance["date"], true ); ?> />
 					<?php _e( 'Show post date' ); ?>
 				</label>
 			</p>
+			
 			<?php if ( function_exists('the_post_thumbnail') && current_theme_supports("post-thumbnails") ) : ?>
 			<p>
 				<label for="<?php echo $this->get_field_id("thumb"); ?>">
